4f2e22eeea3c57938eb384ec2879db8cdee3cf50
Debasis Mahakud <debasismahakud2000@gmail.com>
2025-02-10 11:24:46 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
+        System.out.println("Hello and welcome!");
+        System.out.println("Again changes in IDE");
 
     }
 }
